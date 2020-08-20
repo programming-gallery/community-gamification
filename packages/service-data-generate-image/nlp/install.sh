0d62dd7e1cbd0df16eafd4f903b9158377fb0081
Eunchul <eunchul.dev@gmail.com>
2020-08-20 15:43:42 +0900

    WIP

--- packages/service-data-generate-image/nlp/install.sh
+++ packages/service-data-generate-image/nlp/install.sh
@@ -1,7 +1,6 @@
 #! /bin/bash
-mkdir staging
-cd staging
-tar zxfv ../archives/mecab-0.996-ko-0.9.2.tar.gz
+cd thirdparty
+tar zxfv ../../archives/mecab-0.996-ko-0.9.2.tar.gz
 cd mecab-0.996-ko-0.9.2
 ./configure
 make
@@ -10,9 +9,7 @@ sudo make install
 sudo ldconfig  # In case of error: libmecab.so.2: cannot open shared object file: No such file or directory
 
 cd ..
-tar zxfv ../archives/mecab-0.996-ko-0.9.2.tar.gz
-cd mecab-0.996-ko-0.9.2
+tar zxfv ../../archives/mecab-ko-dic-2.1.1-20180720.tar.gz
+cd mecab-ko-dic-2.1.1-20180720.tar.gz
 ./configure
 make
-sudo make install
-
